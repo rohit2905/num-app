d6b9c55e2b0b844f00c21256e0567a571a13d270
rohit2905 <krohitgoud@gmail.com>
2024-02-08 16:20:35 -0500

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addNum() {
     const ip1 = document.getElementById('inp1').value;
     const ip2 = document.getElementById('inp2').value;
 
-    fetch(`http://arithmetic-service-ybp6.onrender.com/add?num1=${ip1}&num2=${ip2}`)
+    fetch(`http://arithmetic-service-ybp6.onrender.com/add/${ip1}/${ip2}`)
         .then(response => response.text())
         .then(data => {
             document.getElementById('output').innerText = data;
